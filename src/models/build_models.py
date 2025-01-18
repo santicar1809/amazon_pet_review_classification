01a6bd98cfd9526ad02bca665b76b7f52ba23ac1
santicar1809 <santicar1809@gmail.com>
2025-01-18 18:28:11 -0500

    nn

--- src/models/build_models.py
+++ src/models/build_models.py
@@ -4,7 +4,11 @@ from sklearn.metrics import accuracy_score,f1_score,recall_score,precision_score
 import joblib
 import os
 import pandas as pd
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow as tf
 
 def build_models(features_train,features_valid,target_train,target_valid):
     models = all_models()
@@ -17,9 +21,12 @@ def build_models(features_train,features_valid,target_train,target_valid):
         joblib.dump(best,models_path+f'{model[0]}.joblib')
         results.append([model[0],best,accuracy, f1,precision,recall])
     results_df=pd.DataFrame(results,columns=['model','best','accuracy_score','f1_score','precision','recall'])
-    results_df.to_csv('./files/output/results_train.csv',index=False)
     
-    return results_df
+    results_df.to_csv('./files/output/results_train.csv',index=False)
+    results_nn=tens_flow(features_train,features_valid,target_train,target_valid)
+    
+    all_results=pd.concat([results_df,results_nn],ignore_index=True)
+    return all_results
     
 def eval_model(best,features_valid,target_valid):
     preds=best.predict(features_valid)
@@ -38,3 +45,49 @@ def model_structure(features_train,features_valid,target_train,target_valid,mode
     accuracy,f1,precision,recall =eval_model(best_estimator,features_valid,target_valid)
     
     return best_estimator,accuracy,f1,precision,recall
+
+def build_model(data,num_clases):
+    model = Sequential([
+        Dense(128, activation='relu', input_shape=(data,), kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+        Dropout(0.3),  # Dropout for regularization
+        Dense(64, activation='relu', input_shape=(data,), kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+        #Dropout(0.3),  # Dropout for regularization
+        Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+        #Dropout(0.3),  # More dropout for regularization        
+        Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
+        #Dropout(0.3),  # More dropout for regularization        
+        Dense(num_clases, activation='softmax')
+    ])
+    return model
+
+def tens_flow(features_train,features_valid,target_train,target_valid):
+    num_classes = len(set(target_train))
+    seed=12345
+    # Compiling the model
+    model = build_model(features_train.shape[1],num_classes)
+    optimizer = Adam(learning_rate=0.0005)
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    model.summary()
+    # Callbacks
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+
+    # Training the model using GPU if available
+    with tf.device('/GPU:0'):  
+        history = model.fit(features_train, target_train, epochs=200, batch_size=32, 
+                            validation_data=(features_valid, target_valid), callbacks=[early_stopping])
+    results=[]
+    # Evaluating the model
+    preds = model.predict(features_valid)
+    preds_classes = preds.argmax(axis=1)
+    acc=accuracy_score(target_valid,preds_classes)
+    f1=f1_score(target_valid,preds_classes,average='macro')
+    prec=precision_score(target_valid,preds_classes,average='macro')
+    recall=recall_score(target_valid,preds_classes,average='macro')
+    
+    results.append(['Neural_network','',acc,f1,prec,recall])
+    results_df=pd.DataFrame(results,columns=['model','best','accuracy_score','f1_score','precision','recall'])
+    model.save('./models/nn.h5')
+    return results_df
--- src/test/test.py
+++ src/test/test.py
@@ -3,6 +3,17 @@ import joblib
 from src.models.build_models import eval_model
 import scipy.sparse
 from src.models.hyperparameters import all_models
+from tensorflow.keras.models import load_model
+from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score
+
+def eval_nn(model,features_valid,target_valid):
+    preds = model.predict(features_valid)
+    preds_classes = preds.argmax(axis=1)
+    acc=accuracy_score(target_valid,preds_classes)
+    f1=f1_score(target_valid,preds_classes,average='macro')
+    prec=precision_score(target_valid,preds_classes,average='macro')
+    recall=recall_score(target_valid,preds_classes,average='macro')
+    return acc,f1,prec,recall
 
 def test():
     features_test = scipy.sparse.load_npz('./files/output/features_test.npz')
@@ -15,6 +26,14 @@ def test():
         acc,f1,precision,recall=eval_model(test_model,features_test,target_test)
         results.append([model[0],acc,f1,precision,recall])
     results_df=pd.DataFrame(results,columns=['model','accuracy_score','f1_score','precision','recall'])
-    results_df.to_csv('./files/output/results_test.csv',index=False)
-    return results_df
+    
+    results_nn=[]
+    model_nn = load_model('./models/nn.h5')
+    acc,f1,precision,recall=eval_nn(model_nn,features_test,target_test)
+    results_nn.append(['nn',acc,f1,precision,recall])
+    results_df_nn=pd.DataFrame(results_nn,columns=['model','accuracy_score','f1_score','precision','recall'])
+    results_data=pd.concat([results_df,results_df_nn])
+    results_data.to_csv('./files/output/results_test.csv',index=False)
+    
+    return results_data
 test()
